c54246156f09331058db8ec547aba870170567ed
spiderbait <dontwakeupbabys@icloud.com>
2019-08-13 22:53:56 +0800

    hello leetcode commit

--- src/hello_leetcode/HelloLeetcode.java
+++ src/hello_leetcode/HelloLeetcode.java
@@ -3,6 +3,6 @@ package hello_leetcode;
 public class HelloLeetcode {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Leetcode!");
+        System.out.println("Hello, Leetcode!"); //hello leetcode
     }
 }
