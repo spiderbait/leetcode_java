51a8a3a00f39a614f6c373c91306d3963d655920
spiderbait <dontwakeupbabys@icloud.com>
2019-10-08 20:46:27 +0800

    sync part of problems solved before

--- src/S1115_Print_FooBar_Alternately/FooBar.java
+++ src/S1115_Print_FooBar_Alternately/FooBar.java
@@ -4,17 +4,22 @@ import java.util.concurrent.Semaphore;
 
 class FooBar {
     private int n;
+    Semaphore A;
+    Semaphore B;
 
     public FooBar(int n) {
         this.n = n;
+        A = new Semaphore(1);
+        B = new Semaphore(0);
     }
 
     public void foo(Runnable printFoo) throws InterruptedException {
 
         for (int i = 0; i < n; i++) {
-
+//            A.acquire();
             // printFoo.run() outputs "foo". Do not change or remove this line.
             printFoo.run();
+//            B.release();
         }
     }
 
@@ -22,8 +27,10 @@ class FooBar {
 
         for (int i = 0; i < n; i++) {
 
+//            B.acquire();
             // printBar.run() outputs "bar". Do not change or remove this line.
             printBar.run();
+//            A.release();
         }
     }
 
