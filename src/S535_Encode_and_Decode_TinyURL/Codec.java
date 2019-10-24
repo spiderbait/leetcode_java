6f6b45316052417ac14a069e1e46f331e6dc0a22
tiantian <dontwakeupbabys@icloud.com>
2019-10-24 15:11:14 +0800

    solution 535 added

--- src/S535_Encode_and_Decode_TinyURL/Codec.java
+++ src/S535_Encode_and_Decode_TinyURL/Codec.java
@@ -5,33 +5,34 @@ import java.util.HashMap;
 public class Codec {
 
     private int n = 0;
-    HashMap<String, String> urlMapping;
-    char[] convertMapping = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
-            'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-            'Y', 'Z'};
+    private HashMap<String, String> urlMapping;
+    private char[] convertMapping = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+            'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+            'W', 'X', 'Y', 'Z'};
 
-    Codec() {
+    private Codec() {
         this.urlMapping = new HashMap<>();
     }
 
     // Encodes a URL to a shortened URL.
-    public String encode(String longUrl) {
+    private String encode(String longUrl) {
         String shortCode = DecToRadix(this.n);
+        this.n ++;
         this.urlMapping.put(shortCode, longUrl);
         return "http://tinyurl.com/" + shortCode;
     }
 
     // Decodes a shortened URL to its original URL.
-    public String decode(String shortUrl) {
+    private String decode(String shortUrl) {
         String shortCode = shortUrl.split("://")[1].split("/")[1];
         return this.urlMapping.get(shortCode);
     }
 
-    public String DecToRadix(int n) {
+    private String DecToRadix(int n) {
 
         StringBuilder sb = new StringBuilder();
-        int reminder = 0;
+        int reminder;
 
         while(n != 0) {
             reminder = n % 62;
@@ -50,8 +51,10 @@ public class Codec {
     }
 
     public static void main(String[] args) {
+
         Codec codec = new Codec();
-        String shortUrl = codec.encode("https://leetcode.com/problems/encode-and-decode-tinyurl/");
+        String url = "https://leetcode.com/problems/encode-and-decode-tinyurl/";
+        String shortUrl = codec.encode(url);
         String longUrl = codec.decode(shortUrl);
         System.out.println(shortUrl);
         System.out.println(longUrl);
